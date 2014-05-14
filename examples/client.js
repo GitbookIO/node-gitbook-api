ed5f32814abed45bc79cb5684e40e61715ed3e13
Samy Pessé <samypesse@gmail.com>
2014-05-14 22:44:57 +0200

    Add base to publish a book

--- examples/client.js
+++ examples/client.js
@@ -4,9 +4,9 @@ var debug = require("./debug");
 var client = new GitBook({
     host: "http://localhost:5000",
     auth: {
-        username: "GitBookIO",
-        password: "testtest"
+        username: "SamyPesse",
+        password: "2f2274b1-6eb8-4b38-99ce-b4c9119d06ea"
     }
 });
 
-debug(client.account());
+module.exports = client;
